2f009edbf2ae7be6624a67509713ed9b5fd38ef1
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2022-06-10 16:06:45 -0400

    iniciando botão de filtro e gráficos

--- dashboardminehr/src/Components/Dashboard/index.tsx
+++ dashboardminehr/src/Components/Dashboard/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { FilterButton } from '../FilterButton'
 import { GraphicBar } from '../GraphicBar'
 import { GraphicScatter } from '../GraphicScatter'
 
-import { Container, Title } from './styles'
+import { Container, Title, Subtitle } from './styles'
 
 export function Dashboard() {
   return (
     <Container>
       <Title>Dashboard</Title>
+      <Subtitle>Desafio Técnico Frontend</Subtitle>
+      <FilterButton></FilterButton>
       <GraphicBar></GraphicBar>
       <GraphicScatter></GraphicScatter>
     </Container>
---
+++ dashboardminehr/src/Components/GraphicBar/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: #FFFFFF;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+
+  position: absolute;
+  width: 680px;
+  height: 360px;
+  left: 28px;
+  top: 228px;
+`
