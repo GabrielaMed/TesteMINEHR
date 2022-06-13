7aaf4122ffead0d17433702a3721db001e45d27d
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2022-06-13 11:05:04 -0400

    arrumando dados dos graficos usando useSate e useEfffect

--- dashboardminehr/src/Components/Dashboard/index.tsx
+++ dashboardminehr/src/Components/Dashboard/index.tsx
@@ -6,22 +6,42 @@ import { GraphicScatter } from '../GraphicScatter'
 import { Container, HoldTitleAndSub, Title, Subtitle, Graphics } from './styles'
 
 export function Dashboard() {
+  const graphicBarData = {
+    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+    categories: [
+      'South Korea',
+      'Canada',
+      'United Kingdom',
+      'Netherlands',
+      'Italy',
+      'France',
+      'Japan',
+      'United States',
+      'China',
+      'Germany'
+    ]
+  }
+
+  const graphicScatterData = {}
+
   return (
     <>
-    <Container>
-      <HoldTitleAndSub>
-      <Title>Dashboard</Title>
-      <Subtitle>Desafio Técnico Frontend</Subtitle>
-      </HoldTitleAndSub>
+      <Container>
+        <HoldTitleAndSub>
+          <Title>Dashboard</Title>
+          <Subtitle>Desafio Técnico Frontend</Subtitle>
+        </HoldTitleAndSub>
 
-      <FilterButton></FilterButton>
-    </Container>
+        <FilterButton></FilterButton>
+      </Container>
 
-    <Graphics>
-      <GraphicBar></GraphicBar>
-      <GraphicScatter></GraphicScatter>
-    </Graphics>
-
+      <Graphics>
+        <GraphicBar
+          data={graphicBarData.data}
+          categories={graphicBarData.categories}
+        ></GraphicBar>
+        <GraphicScatter></GraphicScatter>
+      </Graphics>
     </>
   )
 }
--- dashboardminehr/src/Components/GraphicBar/index.tsx
+++ dashboardminehr/src/Components/GraphicBar/index.tsx
@@ -1,49 +1,69 @@
-import { useState } from 'react';
-import ReactApexChart from 'react-apexcharts';
+import { ApexOptions } from 'apexcharts'
+import { useEffect, useState } from 'react'
+import ReactApexChart from 'react-apexcharts'
+import { MdInfo } from 'react-icons/md'
 
 import { Container, Title } from './styles'
 
-export function GraphicBar() {
-  const [state, setState] = useState({
-    series: [{
-      data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
-    }],
-    options: {
-      chart: {
-        type: 'bar',
-        height: 350
-      },
-      plotOptions: {
-        bar: {
-          borderRadius: 4,
-          horizontal: true,
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      xaxis: {
-        categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
-          'United States', 'China', 'Germany'
-        ],
-      }
-    },
-  
-  
-  })
+interface IGraphicBarProps {
+  data: Number[]
+  categories: String[]
+}
 
-  
+interface IGraphicProps {
+  series?: ApexAxisChartSeries
+  options?: ApexOptions
+}
+
+export function GraphicBar(props: IGraphicBarProps) {
+  const [graphicSettings, setGraphicSettings] = useState<IGraphicProps>()
+
+  useEffect(() => {
+    const newState = {
+      series: [
+        {
+          data: props.data
+        }
+      ] as ApexAxisChartSeries,
+      options: {
+        chart: {
+          type: 'bar',
+          toolbar: {
+            show: true,
+            color: '#5D405C',
+            offsetY: 0
+          }
+        },
+        plotOptions: {
+          bar: {
+            borderRadius: 4,
+            horizontal: true
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: props.categories
+        }
+      } as ApexOptions
+    }
+
+    setGraphicSettings(newState)
+  }, [props])
+
   return (
     <Container>
-      <Title>
-        Barras
-      </Title>
-      <ReactApexChart 
-      options={state.options} 
-      series={state.series} 
-      type="bar" 
-      height={300} />
+      <Title>Barras</Title>
+      <MdInfo color="#19D7AA" />
+      {graphicSettings?.series && graphicSettings?.options && (
+        <ReactApexChart
+          options={graphicSettings.options}
+          series={graphicSettings.series}
+          type="bar"
+          height={300}
+        />
+      )}
     </Container>
-  );
-
+  )
 }
